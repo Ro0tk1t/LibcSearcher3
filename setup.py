e18586760e9055d4155e4b52f9b0d567e2e174ad
ro0tk1t <1576235794@qq.com>
2020-12-15 17:09:51 +0800

    release v1.0.1

--- setup.py
+++ setup.py
@@ -5,12 +5,18 @@ from setuptools import find_packages, setup
 from LibcSearcher3 import VERSION
 
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='LibcSearcher3',
     version=VERSION,
     url='https://github.com/Ro0tk1t/LibcSearcher3.git',
     description='libc offset database searcher',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Ro0tk1t',
+    author_email='R00tk1t@qq.com',
     packages=find_packages(),
     python_requires='>=3.7',
     include_package_data=True,
